6d043c05b1a4c653f4700cafa5c391f24d83702e
David Galambos <31393112+galambosd@users.noreply.github.com>
2017-08-30 16:35:13 -0500

    For now, remove binary functionality

--- prune_presence_absence.py
+++ prune_presence_absence.py
@@ -2,9 +2,9 @@
 
 import argparse as ap
 
-parser = ap.ArgumentParser(description = 'Prune a presence-absence table to include only certain protein clusters. Takes a *.by P-A binary table and a *.txt list of protein cluster names [PC_*].')
+parser = ap.ArgumentParser(description = 'Prune a presence-absence table to include only certain protein clusters. Takes a *.by P-A table and a *.txt list of protein cluster names [PC_*].')
 
-parser.add_argument('t_file', metavar = 'TABLE', type = ap.FileType('rb'), help = 'Path to the presence-absence table binary file.')
+parser.add_argument('t_file', metavar = 'TABLE', type = ap.FileType('r'), help = 'Path to the presence-absence table file.')
 parser.add_argument('pc_file', metavar = 'CLUSTERS', type = ap.FileType('r'), help = 'Path to the list of protein clusters to be included.')
 parser.add_argument('-o', metavar = 'OUTFILE_NAME', default = False, help = 'Name to give to the new, pruned table.')
 parser.add_argument('-b', action = 'store_true', default = False, help = 'Make both a binary and text output table. Default is text only.')
