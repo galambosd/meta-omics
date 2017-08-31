5cc02b07099268d2e6a67c3ed57da737413378cc
David Galambos <31393112+galambosd@users.noreply.github.com>
2017-08-31 09:02:12 -0500

    ~

--- pan_binary_heatmap.py
+++ pan_binary_heatmap.py
@@ -2,21 +2,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sb
 import argparse as ap
+from numpy import arange
 
 parser = ap.ArgumentParser(description = 'A script to make a binary heatmap out of a presence-absence table.')
 parser.add_argument('XL', metavar = 'EXCEL_FILE', help = "Excel file that has a sheet 'table' with the table and a sheet 'descriptions' with the descriptions.")
 parser.add_argument('-o', metavar = 'PLOT_FILE', default = False, help = 'Full file name for the *.pdf plot.')
 
+cols = []
+for x in arange(2, 72):
+    cols.append(x)
+
 args = parser.parse_args()
 with pd.ExcelFile(args.XL) as xls:
-    data = pd.read_excel(xls, 'table', parse_cols = [2:])
-    print data.keys()
+    data = pd.read_excel(xls, 'table', parse_cols = cols)
     descriptors = pd.read_excel(xls, 'descriptions')
 
 fig = plt.figure(figsize = (7,13))
 ax = fig.add_subplot(1,1,1)
-
-sb.heatmap(data, cbar = False, vmin = 0, vmax = 1, xticklabels = data.keys(), yticklabels = descriptors['Gene name'], ax = ax)
+ax.set_xticklabels(data.keys(), fontsize = 8, rotation = 90)
+ax.set_yticklabels(descriptors['Gene name'], fontsize = 8, rotation = 90, va = 'center')
+sb.heatmap(data, cbar = False, vmin = 0, vmax = 1, ax = ax)
 
 
 if not args.o:
