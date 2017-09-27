067b4b452e78925b84adb8f9456e32e4a235e972
David Galambos <31393112+galambosd@users.noreply.github.com>
2017-09-27 15:03:11 -0500

    Updates

--- pan_binary_heatmap.py
+++ pan_binary_heatmap.py
@@ -3,28 +3,55 @@ import pandas as pd
 import seaborn as sb
 import argparse as ap
 from numpy import arange
+from matplotlib.text import Text
 
+# parser
 parser = ap.ArgumentParser(description = 'A script to make a binary heatmap out of a presence-absence table.')
 parser.add_argument('XL', metavar = 'EXCEL_FILE', help = "Excel file that has a sheet 'table' with the table and a sheet 'descriptions' with the descriptions.")
 parser.add_argument('-o', metavar = 'PLOT_FILE', default = False, help = 'Full file name for the *.pdf plot.')
+parser.add_argument('-c', action = 'store_true', default = False, help = 'Make a hierarchically clustered heatmap too.')
+args = parser.parse_args()
 
+
+# which columns to read -- don't read first 2
 cols = []
-for x in arange(2, 72):
+for x in arange(2, 46):
     cols.append(x)
 
-args = parser.parse_args()
+# read the excel file
 with pd.ExcelFile(args.XL) as xls:
-    data = pd.read_excel(xls, 'table', parse_cols = cols)
+    data = pd.read_excel(xls, 'table', index_col = 0, parse_cols = cols)
     descriptors = pd.read_excel(xls, 'descriptions')
+
+genes = descriptors['Gene name']
+bins = data.keys()[3:]
 
 fig = plt.figure(figsize = (7,13))
 ax = fig.add_subplot(1,1,1)
-ax.set_xticklabels(data.keys(), fontsize = 8, rotation = 90)
-ax.set_yticklabels(descriptors['Gene name'], fontsize = 8, rotation = 90, va = 'center')
-sb.heatmap(data, cbar = False, vmin = 0, vmax = 1, ax = ax)
 
+#ax.set_yticklabels(bins, fontsize = 8)
+ax.set_xticklabels(genes, fontsize = 8, rotation = 90)
+sb.heatmap(data, cbar = False, vmin = 0, vmax = 1, ax = ax, xticklabels = True, yticklabels = True)
+plt.tight_layout(rect = [0.075,0,1,1])
+plt.yticks(rotation = 0, fontsize = 8)
 
 if not args.o:
     plt.savefig('pan_binary_heatmap.pdf')
 else:
     plt.savefig(args.o)
+
+plt.clf()
+plt.cla()
+
+if args.c:
+    fig2 = plt.figure(figsize=(7,17))
+    ax2 = fig2.add_subplot(1,1,1)
+    cg = sb.clustermap(data, figsize = (7,17),method = 'ward', metric = 'euclidean', cbar = False, vmin = 0, vmax = 1, xticklabels = True, yticklabels = True)
+    #plt.tight_layout(rect = [0.075,0,1,1])
+    plt.setp(cg.ax_heatmap.yaxis.get_majorticklabels(), rotation=0, fontsize = 8)
+    plt.setp(cg.ax_heatmap.xaxis.get_majorticklabels(), rotation=90, fontsize = 8)
+
+    if not args.o:
+        cg.savefig('pan_binary_heatmap_clustered.pdf')
+    else:
+        cg.savefig(args.o.rstrip('.pdf')+'_clustered.pdf')
