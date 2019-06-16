1fd8f3a634cc2824ebd671c4102d288055f9cf95
David Galambos <31393112+galambosd@users.noreply.github.com>
2019-06-16 14:29:24 -0700

    commenting

--- spring_2018/KEGG_module_lex_parser.py
+++ spring_2018/KEGG_module_lex_parser.py
@@ -2,9 +2,12 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
+# the number of spaces in the definition corresponds to the number of
+# total reactions in the pathway
 num_space = 1.0
 nums_seen = 1.0
 
+# name the different tokens we might encounter in a KEGG module definition
 tokens = (
     'KOnum',
     'PLUS',
@@ -15,39 +18,48 @@ tokens = (
     'SPACE'
 )
 
+# define what the different tokens look like
+# KOnum is K followed by 5 integers
 t_KOnum = r'K\d\d\d\d\d'
 t_PLUS = r'\+'
 t_MINUS = r'-'
-t_COMMA=r','
-t_LPAREN=r'\('
-t_RPAREN=r'\)'
-t_SPACE='\ '
+t_COMMA = r','
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_SPACE = '\ '
 
+# define an 'order of operations' for reading the definition
 precedence = (
-  ('left', 'SPACE'),
-  ('left', 'RPAREN', 'LPAREN'),
-  ('left', 'COMMA'),
-  ('left', 'PLUS', 'MINUS'),
-  ('left', 'KOnum'),
+    ('left', 'SPACE'),
+    ('left', 'RPAREN', 'LPAREN'),
+    ('left', 'COMMA'),
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'KOnum'),
 )
 
 lexer = lex.lex()
 
 # Get the token map from the lexer.  This is required.
 
+
+# See section 6 (yacc) of this documentation
+# https://www.dabeaz.com/ply/ply.html for an example of how the
+# code below works
 def p_expression_plus(p):
     'expression : expression PLUS expression'
     global nums_seen
-    nums_seen+=1
-    p[0]=(p[1]*(nums_seen-1)+p[3])/nums_seen
+    nums_seen += 1
+    p[0] = (p[1] * (nums_seen - 1) + p[3]) / nums_seen
+
 
 def p_expression_space(p):
     'expression : expression SPACE expression'
     global nums_seen
     global num_space
     nums_seen = 1.0
-    p[0] = p[1]+p[3]
-    num_space+=1
+    p[0] = p[1] + p[3]
+    num_space += 1
+
 
 def p_expression_minus(p):
     'expression : expression MINUS expression'
@@ -55,19 +67,21 @@ def p_expression_minus(p):
     nums_seen = 1.0
     p[0] = p[1]
 
+
 def p_expression_comma(p):
     'expression : expression COMMA expression'
     global nums_seen
     nums_seen = 1.0
-    p[0] = max(p[1],p[3])
+    p[0] = max(p[1], p[3])
 
 
 def p_expression_KOnum(p):
     'expression : KOnum'
     if p[1] in bin_set:
-        p[0]=1.0
+        p[0] = 1.0
     else:
-        p[0]=0.0
+        p[0] = 0.0
+
 
 def p_expression_paren(p):
     'expression : LPAREN expression RPAREN'
@@ -76,8 +90,11 @@ def p_expression_paren(p):
     p[0] = p[2]
 
 # Error rule for syntax errors
+
+
 def p_error(p):
     print("Syntax error in input!")
+
 
 def MCR(module, bin):
     # Build the parser
@@ -89,5 +106,5 @@ def MCR(module, bin):
     nums_seen = 1.0
     bin_set = bin
     result = parser.parse(module)
-    result = result/num_space
+    result = result / num_space
     return result
--- spring_2018/calculate_module_completion.py
+++ spring_2018/calculate_module_completion.py
@@ -7,16 +7,16 @@ import seaborn as sb
 
 
 # import modules file
-modules={}
+modules = {}
 with open(argv[1]) as module_file:
     module_file.readline()
     lines = module_file.readlines()
     for line in lines:
         split = line.split('\t')
-        modules[split[1]]=split[2].rstrip('\n')
+        modules[split[1]] = split[2].rstrip('\n')
 
 # import the bin KO description file
-bin_KO ={}
+bin_KO = {}
 with open(argv[2]) as bin_file:
     lines = bin_file.readlines()
     for line in lines:
@@ -27,10 +27,10 @@ with open(argv[2]) as bin_file:
         for KO in KOs:
             if ("Hypo" in KO) or ('COG' in KO) or ('pfam' in KO):
                 continue
-            for char in ['\'',']','[',' ', '\n']:
+            for char in ['\'', ']', '[', ' ', '\n']:
                 KO = KO.replace(char, '')
             KO_list.append(KO)
-        bin_KO[bin_name]=KO_list
+        bin_KO[bin_name] = KO_list
 
 
 total = pd.DataFrame()
@@ -40,19 +40,23 @@ for module in modules.keys():
     for bin in bin_KO:
         mcr = MCR(modules[module], bin_KO[bin])
         mcr_list.append(mcr)
-    df = pd.DataFrame({module:mcr_list}, index = bin_KO.keys())
+    df = pd.DataFrame({module: mcr_list}, index=bin_KO.keys())
     total = pd.concat([total, df], axis=1)
 
-total.to_csv('MCR_bins_across_pathways.txt', sep = '\t', doublequote=False)
+total.to_csv('MCR_bins_across_pathways.txt', sep='\t', doublequote=False)
 # for every bin in the bin_list, calculate the mcr and append to a list
 # Make a series and then a dataframe, keep appending to the total dataframe
 
-fig2 = plt.figure(figsize=(10,13))
-ax2 = fig2.add_subplot(1,1,1)
-cg = sb.clustermap(total, linewidths = 1, linecolor='black', cmap = 'Greens',figsize = (30, 27),method = 'ward', metric = 'euclidean', cbar = True, vmin = 0, vmax = 1, xticklabels = True, yticklabels = True, row_cluster = True, col_cluster = True)
+fig2 = plt.figure(figsize=(10, 13))
+ax2 = fig2.add_subplot(1, 1, 1)
+cg = sb.clustermap(total, linewidths=1, linecolor='black', cmap='Greens',
+                   figsize=(30, 27), method='ward', metric='euclidean',
+                   cbar=True, vmin=0, vmax=1, xticklabels=True,
+                   yticklabels=True, row_cluster=True, col_cluster=True)
+
 #plt.tight_layout(rect = [0.075,0,1,1])
-plt.setp(cg.ax_heatmap.yaxis.get_majorticklabels(), rotation=0, fontsize = 12)
-plt.setp(cg.ax_heatmap.xaxis.get_majorticklabels(), rotation=90, fontsize = 12)
+plt.setp(cg.ax_heatmap.yaxis.get_majorticklabels(), rotation=0, fontsize=12)
+plt.setp(cg.ax_heatmap.xaxis.get_majorticklabels(), rotation=90, fontsize=12)
 # print it at the end into a tab-separated file
 
 cg.savefig(argv[3])
